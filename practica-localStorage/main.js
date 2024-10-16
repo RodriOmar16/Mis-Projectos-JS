75c66aa89d279f87b2ad5f132c4934ff96679c56
Rodrigo <rodrigoomarmiranda1@gmail.com>
2024-10-15 22:30:28 -0300

    15102024_R7

--- practica-localStorage/main.js
+++ practica-localStorage/main.js
@@ -1,1 +1,52 @@
-console.log("funciona");
+const formulario   = document.querySelector("#formulario");
+const pintarTodo   = document.querySelector("#pintarTodo");
+const templateTodo = document.querySelector("#templateTodo").content;
+let todos          = [];
+let idNota         = 0;
+
+const agregarNota = (nombre) => {
+  idNota++;
+  todos.push({
+    id:     idNota,
+    nombre: nombre
+  });
+}
+
+function pintarNotas(){
+  pintarTodo.textContent = ''; //se limpia todo todo irá
+  const fragment = document.createDocumentFragment();
+
+  todos.forEach(e => {
+    const clone = templateTodo.cloneNode(true);
+    clone.querySelector("p").textContent     = e.nombre;
+    clone.querySelector("button").dataset.id = e.id;
+    fragment.append(clone);
+  });
+
+  pintarTodo.appendChild(fragment);
+}
+
+formulario.addEventListener("submit", async e => {
+  e.preventDefault();
+  const data   = new FormData(formulario);
+  const [todo] = [...data.values()];
+
+  const alerta = document.querySelector(".alert-danger");
+  if(!todo.trim()){
+    alerta.className = 'alert alert-danger';
+    return 
+  }else alerta.className = 'alert alert-danger d-none';
+
+  agregarNota(todo);
+  pintarNotas();
+
+  formulario.querySelector("input").value = '';
+})
+
+document.addEventListener("click", e => {
+  if(e.target.dataset.id){
+    const pos = todos.map(e => e.id).indexOf(parseInt(e.target.dataset.id))
+    todos.splice(pos,1);
+    pintarNotas();
+  }
+});
